129d22fd0e6b7a1be1d508e9e6d21441053765b4
JosephSimonPrudente <prudentejoseph3@gmail.com>
2023-02-17 02:17:25 +0800

    defining the things needed for the game

--- Modified Final Project.py
+++ Modified Final Project.py
@@ -6,3 +6,10 @@ root = Tk()
 root.geometry('800x600+400+100')
 root.title('Speed Typing Game')
 root.configure(bg='black')
+
+score = 0
+miss = 0
+timer = 60
+count = 0
+sliderwords = ''
+score_to_beat = 0
